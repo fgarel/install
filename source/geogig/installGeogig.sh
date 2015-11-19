6d184073ff1dd2b9323572f7cd5b1f8f404e2157
Frédéric Garel <frederic.garel@gmail.com>
2015-11-19 18:44:03 +0100

    installation de postgresql

--- source/geogig/installGeogig.sh
+++ source/geogig/installGeogig.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 echo "#"
-echo "# Téléchargement de la dernière version"
+echo "# Installation de spatialite-gui sqlitebrowser et sqlite3"
+echo "#"
+echo "# sudo apt-get install spatialite-gui sqlitebrowser sqlite3"
+sudo apt-get install spatialite-gui sqlitebrowser sqlite3
+echo "#"
+echo "# Installation de geogig : Téléchargement de la dernière version"
 echo "#"
 echo "# wget http://sourceforge.net/projects/geogig/files/latest/download"
 wget http://sourceforge.net/projects/geogig/files/latest/download \
@@ -19,7 +24,11 @@ unzip geogig-cli-app.zip
 echo "#"
 echo "# Ajout de geogig/bin dans le PATH"
 echo "#"
+echo "#-----------------------------------------------------------------------"
+echo "echo \$PATH"
 echo "export PATH=\$PATH:/home/fred/Documents/install/source/geogig/geogig/bin"
 echo "echo \$PATH"
 export PATH=$PATH:/home/fred/Documents/install/source/geogig/geogig/bin
 echo $PATH
+echo "#-----------------------------------------------------------------------"
+echo "#"
