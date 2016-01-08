43ce10a9750575b6d85c813dceb9f06488814281
Frédéric Garel <frederic.garel@gmail.com>
2016-01-08 17:40:10 +0100

    osm

--- source/environnementTravail/osmImportFirstTime2.sh
+++ source/environnementTravail/osmImportFirstTime2.sh
@@ -9,61 +9,71 @@
 echo "# 8ème Partie - Import, la première fois, des données de la région poitou-charentes de pbf vers postgis"
 echo "###############################################"
 echo "#"
-echo "# la doc est ici"
+echo "# La doc est ici"
 echo "# http://wiki.openstreetmap.org/wiki/Osmosis/Detailed_Usage_0.44#PostGIS_Tasks_.28Snapshot_Schema.29"
 echo "# http://wiki.openstreetmap.org/wiki/Osmosis/PostGIS_Setup"
 echo "#"
 echo "# On installe d'abord l'extension hstore sur la base osm"
 echo "#"
-echo "# sudo -u postgres psql --username=postgres \\"
-echo "#                       --dbname=osm \\"
-echo "#                       -c \"CREATE EXTENSION hstore;\""
-sudo -u postgres psql --username=postgres \
-                      --dbname=osm \
-                      -c "CREATE EXTENSION hstore;"
+echo "sudo -u postgres \\"
+echo "     psql --username=postgres \\"
+echo "          --dbname=osm \\"
+echo "          -c \"CREATE EXTENSION hstore;\""
+      sudo -u postgres \
+           psql --username=postgres \
+                --dbname=osm \
+                -c "CREATE EXTENSION hstore;"
 echo "#"
 echo "# Puis on execute quelques scripts pour créer des tables dans la base"
 echo "#"
-echo "# sudo gunzip /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6.sql.gz"
-sudo gunzip /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6.sql.gz
+echo "sudo gunzip /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6.sql.gz"
+      sudo gunzip /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6.sql.gz
 echo "#"
-echo "# sudo -u www-data psql --username=www-data \\"
-echo "#                       --dbname=osm \\"
-echo "#                       -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6.sql"
-sudo -u www-data psql --username=www-data \
-                      --dbname=osm \
-                      -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6.sql
+echo "sudo -u www-data \\"
+echo "     psql --username=www-data \\"
+echo "          --dbname=osm \\"
+echo "          -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6.sql"
+      sudo -u www-data \
+           psql --username=www-data \
+                --dbname=osm \
+                -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6.sql
 echo "#"
-echo "# sudo -u www-data psql --username=www-data \\"
-echo "#                       --dbname=osm \\"
-echo "#                       -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_action.sql"
-sudo -u www-data psql --username=www-data \
-                      --dbname=osm \
-                      -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_action.sql
+echo "sudo -u www-data \\"
+echo "     psql --username=www-data \\"
+echo "          --dbname=osm \\"
+echo "          -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_action.sql"
+      sudo -u www-data \
+           psql --username=www-data \
+                --dbname=osm \
+                -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_action.sql
 echo "#"
-echo "# sudo -u www-data psql --username=www-data \\"
-echo "#                       --dbname=osm \\"
-echo "#                       -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_bbox.sql"
-sudo -u www-data psql --username=www-data \
-                      --dbname=osm \
-                      -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_bbox.sql
+echo "sudo -u www-data \\"
+echo "     psql --username=www-data \\"
+echo "          --dbname=osm \\"
+echo "          -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_bbox.sql"
+      sudo -u www-data \
+           psql --username=www-data \
+                --dbname=osm \
+                -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_bbox.sql
 echo "#"
-echo "# sudo -u www-data psql --username=www-data \\"
-echo "#                       --dbname=osm \\"
-echo "#                       -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_linestring.sql"
-sudo -u www-data psql --username=www-data \
-                      --dbname=osm \
-                      -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_linestring.sql
+echo  sudo -u www-data \\"
+echo "     psql --username=www-data \\"
+echo "     --dbname=osm \\"
+echo "     -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_linestring.sql"
+      sudo -u www-data \
+           psql --username=www-data \
+                --dbname=osm \
+                -f /usr/share/doc/osmosis/examples/pgsnapshot_schema_0.6_linestring.sql
+echo "#"
 echo "#"
 echo "#"
 echo "#"
 echo "# On va importer les données de la région poitou-charentes"
+echo "# grace à osmosis"
 echo "#"
-echo "# osmosis --read-pbf /home/fred/Documents/osmosis/poitou-charentes-latest.osm.pbf \\"
-echo "#         --write-pgsql host=\"localhost\" database=\"osm\" user=\"www-data\" password=\"www-data\""
+echo "osmosis --read-pbf /home/fred/Documents/osmosis/poitou-charentes-latest.osm.pbf \\"
+echo "        --write-pgsql host=\"localhost\" database=\"osm\" user=\"www-data\" password=\"www-data\""
+      osmosis --read-pbf /home/fred/Documents/osmosis/poitou-charentes-latest.osm.pbf \
+              --write-pgsql host="localhost" database="osm" user="www-data" password="www-data"
 echo "#"
-osmosis --read-pbf /home/fred/Documents/osmosis/poitou-charentes-latest.osm.pbf \
-        --write-pgsql host="localhost" database="osm" user="www-data" password="www-data"
-echo "#"
-
-echo "#"
+echo ""
--- source/environnementTravail/osmImportOtherTime.sh
+++ source/environnementTravail/osmImportOtherTime.sh
@@ -10,15 +10,30 @@ echo "# 8ème Partie - Import, les fois suivantes, des données de la région po
 echo "###############################################"
 echo "#"
 echo "#"
+echo "# Affichage de l'ancienne date"
+echo "cat /home/fred/Documents/osmosis/state.txt"
+      cat /home/fred/Documents/osmosis/state.txt
+echo "#"
 echo "#"
 echo "# On va importer les mises a jour des données de la région poitou-charentes"
 echo "#"
-echo "# osmosis --read-pbf /home/fred/Documents/osmosis/poitou-charentes-latest.osm.pbf \\"
-echo "#         --write-pgsql host=\"localhost\" database=\"osm\" user=\"www-data\" password=\"www-data\""
-echo "#"
+#echo "osmosis --read-pbf /home/fred/Documents/osmosis/poitou-charentes-latest.osm.pbf \\"
+#echo "        --write-pgsql host=\"localhost\" database=\"osm\" user=\"www-data\" password=\"www-data\""
 #osmosis --read-pbf /home/fred/Documents/osmosis/poitou-charentes-latest.osm.pbf \
 #        --write-pgsql host="localhost" database="osm" user="www-data" password="www-data"
 # sudo -u www-data osm2pgsl -a changes.osc.gz -e11-17 -o expired-tiles.txt
+echo "#"
+echo "osmosis --read-xml-change \\"
+echo "        /home/fred/Documents/osmosis/change.osc.gz \\"
+#echo "        --simplify-change \\"
+echo "        --write-pgsql-change host=\"localhost\" database=\"osm\" user=\"www-data\" password=\"www-data\""
+      osmosis --read-xml-change \
+              /home/fred/Documents/osmosis/change.osc.gz \
+              --write-pgsql-change host=\"localhost\" database=\"osm\" user=\"www-data\" password=\"www-data\"
+echo "#"
+echo "# Affichage de la nouvelle date"
+echo "cat /home/fred/Documents/osmosis/state.txt"
+      cat /home/fred/Documents/osmosis/state.txt
 echo "#"
 
 echo "#"
