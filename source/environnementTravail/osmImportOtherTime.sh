f2fe19bdfc5bd04f2e9613e2a32652b207ed9a0d
Frédéric Garel <frederic.garel@gmail.com>
2016-01-11 18:57:43 +0100

    import modifie pour l'utilisateur Fred

--- source/environnementTravail/osmImportOtherTime.sh
+++ source/environnementTravail/osmImportOtherTime.sh
@@ -33,22 +33,26 @@ echo "#"
 echo "osmosis --read-xml-change \\"
 echo "        /home/fred/Documents/osmosis/change.osc.gz \\"
 #echo "        --simplify-change \\"
-echo "        --write-pgsql-change host=\"localhost\" database=\"osm\" user=\"www-data\" password=\"www-data\""
+echo "        --write-pgsql-change host=\"localhost\" database=\"osm\" user=\"Fred\" password=\"Fred\""
       osmosis --read-xml-change \
               /home/fred/Documents/osmosis/change.osc.gz \
-              --write-pgsql-change host="localhost" database="osm" user="www-data" password="www-data"
+              --write-pgsql-change host="localhost" database="osm" user="Fred" password="Fred"
 echo "#"
 echo "# Affichage de la nouvelle date"
 echo "cat /home/fred/Documents/osmosis/state.txt"
       cat /home/fred/Documents/osmosis/state.txt
 echo "#"
 echo "# On interroge la base apres import"
-echo "sudo -u www-data \\"
-echo "     psql --username=www-data \\"
-echo "          --dbname=osm \\"
-echo "          -f analyse_base_osm.sql"
-      sudo -u www-data \
-           psql --username=www-data \
-                --dbname=osm \
-                -f analyse_base_osm.sql
+echo 'psql \'
+echo '     --host=localhost \'
+echo '     --dbname=osm \'
+echo '     --username=Fred \'
+echo '     --no-password \'
+echo '     -f analyse_base_osm.sql'
+      psql \
+           --host=localhost \
+           --dbname=osm \
+           --username=Fred \
+           --no-password \
+           -f analyse_base_osm.sql
 echo "#"
