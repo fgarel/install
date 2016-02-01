c35f533fbcfefd2a5ec914ced89dd67934248620
Frédéric Garel <frederic.garel@gmail.com>
2016-02-01 09:58:41 +0100

    modif osmMaj

--- source/environnementTravail/osmImportOtherTime.sh
+++ source/environnementTravail/osmImportOtherTime.sh
@@ -4,14 +4,14 @@
 
 echo "# OsmImportOtherTime.sh"
 echo "###############################################"
+#echo "#"
+#echo "#"
+#echo "# Affichage de l'ancienne date"
+#echo "cat /home/fred/Documents/osmosis/state.txt"
+#      cat /home/fred/Documents/osmosis/state.txt
 echo "#"
-echo "#"
-echo "# Affichage de l'ancienne date"
-echo "cat /home/fred/Documents/osmosis/state.txt"
-      cat /home/fred/Documents/osmosis/state.txt
-echo "#"
-echo "# On va importer les mises a jour des données"
-echo "#"
+echo "# Import du fichier diff \"quotidien\" dans la base"
+#echo "#"
 #echo "# La version avec l'outil osm2psql au lieu et a la place de osmosis est détaillée ici"
 #echo "# sudo -u www-data osm2pgsl -a changes.osc.gz -e11-17 -o expired-tiles.txt"
 # sudo -u www-data osm2pgsl -a changes.osc.gz -e11-17 -o expired-tiles.txt
@@ -28,4 +28,4 @@ echo "# Affichage de la nouvelle date"
 echo "cat /home/fred/Documents/osmosis/state.txt"
       cat /home/fred/Documents/osmosis/state.txt
 echo "#"
-echo "#"
+#echo "#"
