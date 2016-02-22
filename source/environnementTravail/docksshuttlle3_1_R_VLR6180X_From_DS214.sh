51357fe0249b5e203fd868bf93ff0e02739bebf2
Frédéric Garel <frederic.garel@gmail.com>
2016-02-22 18:38:33 +0100

    modif de docksshuttle3_*

--- source/environnementTravail/docksshuttlle3_1_R_VLR6180X_From_DS214.sh
+++ source/environnementTravail/docksshuttlle3_1_R_VLR6180X_From_DS214.sh
@@ -9,4 +9,7 @@ ssh -f\
     -R 25900:localhost:15900 \
     -R 20080:localhost:10080 \
     -R 20022:localhost:10022 \
+    -R 15900:localhost:5900 \
+    -R 10080:localhost:80 \
+    -R 10022:localhost:22 \
     root@82.245.10.108
--- source/environnementTravail/docksshuttlle3_3_L_Tux0_To_DS214.sh
+++ source/environnementTravail/docksshuttlle3_3_L_Tux0_To_DS214.sh
@@ -9,4 +9,10 @@ ssh -f\
     -L 35900:localhost:25900 \
     -L 30080:localhost:20080 \
     -L 30022:localhost:20022 \
+    -L 25900:localhost:15900 \
+    -L 20080:localhost:10080 \
+    -L 20022:localhost:10022 \
+    -L 15900:localhost:5900 \
+    -L 10080:localhost:80 \
+    -L 10022:localhost:22 \
     root@192.168.0.26
