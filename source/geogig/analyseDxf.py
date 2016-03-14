aa4e29521ac784593d1ca2e8fd70485038187576
Frédéric Garel <frederic.garel@gmail.com>
2016-03-14 17:17:52 +0100

    analyseDxf

--- source/geogig/analyseDxf.py
+++ source/geogig/analyseDxf.py
@@ -4,7 +4,11 @@
 #import csv
 #with open('test.dxf', 'r') as inputfile:
 #with open("/home/fred/f/CARTOGRAPHIE/Plans/1_Topo_Existants/4_Plans_conformes_CC46/Ottawa-rochepierre-créches/100097_Rue d\'ottawa-Rue de Rochepierre_Topo 2016.dxf", 'r') as inputfile:
-outputfile_01_HEADER = open('dataout_01_HEADER.txt', 'w')
+#outputfile_01_HEADER = open('dataout_01_HEADER.txt', 'w')
+
+#from dictionnaireOrdonne import DictionnaireOrdonne
+#from string import Template
+
 
 with open("original.dxf", 'r') as inputfile:
     #with open('dataout.txt', 'w') as outputfile:
@@ -19,7 +23,7 @@ with open("original.dxf", 'r') as inputfile:
         for line in inputfile:
             compteur_ligne += 1
             #encoded_1 = line.encode()
-            encoded_2 = line.encode('latin1').decode('latin1').strip()
+            encoded_2 = line.encode('latin1').decode('latin1')[:-1]
             #encoded_3 = line.encode('latin1').decode('utf-8')
             #encoded_4 = line.encode('utf-8').decode('latin1')
             #encoded_5 = line.encode('utf-8').decode('utf-8')
@@ -33,63 +37,76 @@ with open("original.dxf", 'r') as inputfile:
                 outputfile.write('{0}£{1}\n'.format(ligne_en_stock, encoded_2))
                 #csvwriter = csv.writer(outputfile, delimiter='-',lineterminator='\n')
             else:
-                ligne_en_stock = line.strip()
+                ligne_en_stock = line[:-1]
         #    csvwriter.writerow(row)
     #       close csvoutputfile
     outputfile.close()
 inputfile.close()
 
 
-list_sections={'HEADER': 'dataout_01_HEADER.txt', \
-               'CLASSES': 'dataout_02_CLASSES.txt', \
-               'TABLES': 'dataout_03_TABLES.txt', \
-               'BLOCKS': 'dataout_04_BLOCKS.txt', \
-               'ENTITIES': 'dataout_05_ENTITIES.txt', \
-               'OBJECTS': 'dataout_06_OBJECTS.txt'}
+list_sections={'dataout_01_HEADER.txt': 'HEADER', \
+               'dataout_02_CLASSES.txt': 'CLASSES', \
+               'dataout_03_TABLES.txt': 'TABLES', \
+               'dataout_04_BLOCKS.txt': 'BLOCKS', \
+               'dataout_05_ENTITIES.txt': 'ENTITIES', \
+               'dataout_06_OBJECTS.txt': 'OBJECTS'}
+
+
 with open('dataout.txt', 'r') as inputfile:
-    section_en_cours=list_sections[0]
-    list_sections.remove(section_en_cours)
-    outputfile = open('dataout_01_HEADER.txt', 'w')
+    # dans l'ordre des fichers a écrire,
+    # creation de deux listes
+    cles_triees = sorted(list_sections.keys())
+    valeurs_triees = []
+    for cle in cles_triees:
+        valeurs_triees.append(list_sections[cle])
+
+    
+    outputfile = open(cles_triees[0], 'w')
+    ligne_sauvegardee = ''
+    numero_ligne=0
     for line in inputfile:
+        numero_ligne+=1
         code, valeur = line.split('£')
         valeur = valeur[:-1]
-        if (code != 0 or valeur != 'SECTION'):
-            #print('{0}'.format(line))
-            print('{0} {1}'.format(code, valeur))
+
+        if (ligne_sauvegardee == '' and valeur != 'EOF' and (code != '  0' or valeur != 'SECTION')):
+            if numero_ligne>228800:
+                #print('{0} {1}'.format(code, valeur))
+                print(code, valeur)
             outputfile.write('{0}£{1}\n'.format(code, valeur))
-        elif (code == 0 and valeur == 'SECTION' and section_en_cours == ''):
-            section_en_cours='HEADER'
-            outputfile = open('dataout_01_HEADER.txt', 'w')
-            print('-------------------------------HEADER')
-        elif (code == 0 and valeur == 'SECTION' and section_en_cours == 'HEADER'):
+
+        if (code == '  0' and valeur == 'SECTION'):
             outputfile.close()
-            section_en_cours='CLASSES'
-            outputfile = open('dataout_02_CLASSES.txt', 'w')
-            print('-------------------------------CLASSES')
-        elif (code == 0 and valeur == 'SECTION' and section_en_cours == 'CLASSES'):
+            #print('attention : changement de section')
+            ligne_sauvegardee = '{0}£{1}\n'.format(code, valeur)
+            continue
+            
+        if (ligne_sauvegardee != '' and code == '  2' and valeur in valeurs_triees):
+            #print('attention : changement de section 2 ')
+            section_en_cours=valeur
+            for cle in cles_triees:
+                if list_sections[cle] == valeur:
+                    outputfile = open(cle, 'w')
+                    #print(ligne_sauvegardee)
+                    outputfile.write(ligne_sauvegardee)
+                    ligne_sauvegardee=''
+                    #print('{0} {1}'.format(code, valeur))
+                    outputfile.write('{0}£{1}\n'.format(code, valeur))
+                    break
+                
+        if (code == '  0' and valeur == 'EOF'):
             outputfile.close()
-            section_en_cours='TABLES'
-            outputfile = open('dataout_03_TABLES.txt', 'w')
-            print('-------------------------------TABLES')
-        elif (code == 0 and valeur == 'SECTION' and section_en_cours == 'TABLES'):
-            outputfile.close()
-            section_en_cours='BLOCKS'
-            outputfile = open('dataout_04_BLOCKS.txt', 'w')
-            print('-------------------------------BLOCKS')
-        elif (code == 0 and valeur == 'SECTION' and section_en_cours == 'BLOCKS'):
-            outputfile.close()
-            section_en_cours='ENTITIES'
-            outputfile = open('dataout_05_ENTITIES.txt', 'w')
-            print('-------------------------------ENTITIES')
-        elif (code == 0 and valeur == 'SECTION' and section_en_cours == 'ENTITIES'):
-            outputfile.close()
-            section_en_cours='OBJECTS'
-            outputfile = open('dataout_06_OBJECTS.txt', 'w')
-            print('-------------------------------OBJECTS')
-        elif (code == 0 and valeur == 'EOF' and section_en_cours == 'OBJECTS'):
-            outputfile.close()
-            print('-------------------------------EOF')
-        #outputfile.write('{0}'.format(line))
+    inputfile.close()
     #outputfile.close()
-inputfile.close()
-#fic.close()
+
+
+a = DictionnaireOrdonne(list_sections)
+#list_sections=DictionnaireOrdonne( \
+#               'HEADER'= 'dataout_01_HEADER.txt', \
+#               'CLASSES'= 'dataout_02_CLASSES.txt', \
+#               'TABLES'= 'dataout_03_TABLES.txt', \
+#               'BLOCKS'= 'dataout_04_BLOCKS.txt', \
+#               'ENTITIES'= 'dataout_05_ENTITIES.txt', \
+#               'OBJECTS'= 'dataout_06_OBJECTS.txt')
+#print(a)
+#print(a.sort)
