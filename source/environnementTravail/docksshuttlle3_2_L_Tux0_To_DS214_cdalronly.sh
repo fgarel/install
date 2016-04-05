5cc802409d3bec1be83aadadeb578b4523bc7df9
Frédéric Garel <frederic.garel@gmail.com>
2016-04-05 17:25:27 +0200

    ajout du port 5432 dans les tunnels

--- source/environnementTravail/docksshuttlle3_2_L_Tux0_To_DS214_cdalronly.sh
+++ source/environnementTravail/docksshuttlle3_2_L_Tux0_To_DS214_cdalronly.sh
@@ -20,13 +20,13 @@
 #    -L 10022:localhost:22 \
 #    root@192.168.0.26
 #
-echo "# "
-echo "# Maintenant, il est possible de se connecter"
-echo "# DS214    => ssh -p 10022 root@localhost"
-echo "# VLR6180X => ssh -p 20022 fred@localhost"
-echo "# (si besoin, il faut réveiller VLR6180Y)"
-echo "# (et faire le 3ème tiers du tunnel)"
-echo "# VLR6180Y => ssh -p 30022 fred@localhost"
+#echo "# "
+#echo "# Maintenant, il est possible de se connecter"
+#echo "# DS214    => ssh -p 10022 root@localhost"
+#echo "# VLR6180X => ssh -p 20022 fred@localhost"
+#echo "# (si besoin, il faut réveiller VLR6180Y)"
+#echo "# (et faire le 3ème tiers du tunnel)"
+#echo "# VLR6180Y => ssh -p 30022 fred@localhost"
 #
 # Fabrication du premier tiers du tunnel "mail.cdalr.fr" (entre la machine perso et mail.cdalr.fr)
 #
@@ -35,12 +35,15 @@ ssh -f \
     -N \
     -X \
     -L 64900:localhost:55900 \
+    -L 64432:localhost:55432 \
     -L 60080:localhost:50080 \
     -L 60022:localhost:50022 \
     -L 55900:localhost:45900 \
+    -L 55432:localhost:45432 \
     -L 50080:localhost:40080 \
     -L 50022:localhost:40022 \
     -L 45900:localhost:5900 \
+    -L 45432:localhost:5432 \
     -L 40080:localhost:80 \
     -L 40022:localhost:22 \
     fred@mail.cdalr.fr
