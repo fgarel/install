c958c07195e2bd441e17a82960dc985a654c8290
Frédéric Garel <frederic.garel@gmail.com>
2016-04-18 16:00:17 +0200

    modif de osmTest.sh

--- source/environnementTravail/osmTest.sh
+++ source/environnementTravail/osmTest.sh
@@ -17,11 +17,12 @@ echo '     --host=localhost \'
 echo '     --dbname=osm \'
 echo '     --username=Fred \'
 echo '     --no-password \'
-echo '     -f analyse_base_osm.sql'
+echo '     -f ~fred/Documents/install/source/environnementTravail/analyse_base_osm.sql'
+#echo '     -f analyse_base_osm.sql'
       psql \
            --host=localhost \
            --dbname=osm \
            --username=Fred \
            --no-password \
-           -f analyse_base_osm.sql
+           -f ~fred/Documents/install/source/environnementTravail/analyse_base_osm.sql
 echo "#"
