556943d306ed671eaaf9a9c9c7663a927cf3524d
Frédéric Garel <frederic.garel@gmail.com>
2017-02-22 22:16:58 +0100

    install vpngate

---
+++ source/environnementTravail/installVpnGateWithProxy.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "#"
+echo "# regarder des videos"
+echo "# Le but est d'installer vpngate et dont apnic"
+echo "#"
+echo "# La doc sur vpngate est ici :"
+echo "# http://www.vpngate.net/en/"
+echo "# https://github.com/Dragon2fly/vpngate-with-proxy"
+echo "#"
+echo "# La doc sur dontpanic est ici :"
+echo "# https://yeuxdelibad.net/Blog/?d=2017/02/10/13/33/05-dontpanic-passe-en-version-20"
+echo "# http://3hg.toile-libre.org/dontpanic/index.html"
+echo "# http://3hg.toile-libre.org/dontpanic/download.html"
+echo "#"
+echo "# Alternative :"
+echo "# https://touchandgo.readthedocs.io/en/master/"
+echo "#"
+echo "# Installation : clonage du depot vpngate-with-proxy"
+echo "git clone https://github.com/Dragon2fly/vpngate-with-proxy.git"
+      git clone https://github.com/Dragon2fly/vpngate-with-proxy.git
+echo "# First run : "
+echo "cd vpngate-with-proxy"
+      cd vpngate-with-proxy
+echo "#./run tui"
+      #./run tui
+echo "#"
