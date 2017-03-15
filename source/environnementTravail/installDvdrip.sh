813f2fe654ed83c76c2539d26b913144b00266ce
Frédéric Garel <frederic.garel@gmail.com>
2017-03-15 14:33:26 +0100

    freevpn

---
+++ source/environnementTravail/installDvdrip.sh
@@ -0,0 +1,9 @@
+installation de dvdrip
+
+git clone https://github.com/xenomachina/dvdrip
+
+sudo aptitude install handbrake-cli
+
+Utilisation 
+
+sudo ./dvdrip.py -i /media/fred/UTOPIA_S1_DVD1 -o Utopia_s1_dvd1
