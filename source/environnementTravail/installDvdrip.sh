8ab97d21b6f51363a6c050ba318cd74454e07b6d
Frédéric Garel <frederic.garel@gmail.com>
2017-03-24 18:13:45 +0100

    dvdrip

--- source/environnementTravail/installDvdrip.sh
+++ source/environnementTravail/installDvdrip.sh
@@ -1,4 +1,15 @@
-installation de dvdrip
+# la copie de dvd....
+
+# methode 1 : handbrake-gui
+# methode 2 : dvdrip2
+# methode 3 : vlc / lecture conversion
+# methode 4 : dvdrip
+# methode 5 : dvdisaster
+# methode 6 : vobcopy
+# methode 7 : rsync -av
+
+
+# installation de dvdrip22
 
 git clone https://github.com/xenomachina/dvdrip
 
@@ -15,7 +26,14 @@ rm -rf ~/Videso/HonourableWoman ; \
             -i /dev/sr0 \
             -o ~/Videos/HonourableWoman
 
-# methode 1 handbrake-gui
-# methode 2 : dvdrip2
-# methode 3 : vlc / lecture conversion
-# methode 4 : dvdrip
+# installation de dvdisater
+# vu ici
+# https://superuser.com/questions/391646/how-to-copy-a-dvd-with-a-patched-udf-filesystem-bad-sectors/392506#392506
+# http://dvdisaster.net/downloads/manual.pdf
+aptitude install dvdisaster
+utilisation
+dvdisaster -d /dev/sr0 \
+           -i ~/Videos/HonourableWoman.iso \
+           -e ~/Videos/corr.ecc \
+           -r \
+           --adaptive-read
