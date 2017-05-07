ec0edff249714497cb634265e91efd38f4529c3f
Frédéric Garel <frederic.garel@gmail.com>
2017-05-07 11:20:46 +0200

    ssh_wake

---
+++ source/environnementTravail/ssh_wake_vlr6180x.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# fgarel@freeshell.de:~/lisezMoi.txt
+
+#
+# Options de ssh
+# http://www.delafond.org/traducmanfr/man/man1/ssh.1.html
+# https://mosh.mit.edu/
+#
+# -f = Demande à ssh de basculer en arrière-plan juste avant d'exécuter la commande.
+# -o ServerAliveInterval = http://stackoverflow.com/questions/25084288/keep-ssh-session-alive
+#                          https://forum.ivorde.com/how-to-prevent-linux-ssh-client-from-disconnecting-using-serveraliveinterval-t19451.html
+#                          https://mosh.mit.edu/
+# -N = N'exécute aucune commande distante
+# -X = Transfert X11 et TCP (X11 and TCP forwarding) http://www.delafond.org/traducmanfr/man/man1/ssh.1.html
+
+#
+#
+# Reveil de tux0
+#
+echo "#"
+echo "#"
+echo "# Attention, ce script est a executer sur le reseau local de"
+echo "# la machine à reveiller"
+echo "#"
+echo "#"
+echo "# Reveil de la machine..."
+echo "# "
+# 
+#sudo synonet --wake 00:16:17:CD:2A:A7 eth0
+# vlr6180x = 78:45:c4:27:54:02
+wakeonlan 78:45:c4:27:54:02
+# vlr6180y = 18:03:73:2f:3b:7d
+#wakeonlan 18:03:73:2f:3b:7d
+#
+# attente du ping
+#
+# http://serverfault.com/questions/42021/how-to-ping-in-linux-until-host-is-known
+#
+echo "# "
+echo "# On attend que la machine reponde au ping..."
+echo "# "
+#
+#
+#until sudo ping -c1 192.168.1.45 &>/dev/null; do :; done
+until sudo ping -c1 vlr6180x &>/dev/null; do :; done
+
+#
+# sur tux0, il faut lancer l'ouverture du demi-tunnel
+#
+# http://unix.stackexchange.com/questions/119894/single-command-to-login-to-ssh-and-run-program
+#
+echo "# "
+echo "# Sur la machine distante, on lance son ouverture (demi-tunnel)"
+echo "# "
+#
+ssh -t vlr6180x ./ssh_open_host_vlr6180x.sh 
